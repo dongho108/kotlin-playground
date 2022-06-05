0f09824069d706f8591da3248c769b5c2f4b0364
dongho <dongho108@naver.com>
2022-06-06 02:15:13 +0900

    feat: 확장 프로퍼티 예제 추가

--- src/main/kotlin/kotlinInAction/chapter3/ExpandProperty.kt
+++ src/main/kotlin/kotlinInAction/chapter3/ExpandProperty.kt
@@ -1,4 +1,16 @@
 package kotlinInAction.chapter3
 
-class ExpandProperty {
+fun main() {
+    val sb = StringBuilder("hello")
+    println(sb.lastChar)
+    sb.lastChar = '!'
+}
+
+var StringBuilder.lastChar: Char
+    get() = get(length - 1)
+    set(value: Char) {
+        this.setCharAt(length - 1, value)
+    }
+
+class ExpandProperty() {
 }
