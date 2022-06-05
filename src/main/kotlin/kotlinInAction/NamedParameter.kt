ed996f8e6f113fc5c33e047e05b10c3862600cb6
dongho <dongho108@naver.com>
2022-06-06 01:26:51 +0900

    Initial commit

---
+++ src/main/kotlin/kotlinInAction/NamedParameter.kt
@@ -0,0 +1,33 @@
+package kotlinInAction
+
+import java.lang.StringBuilder
+
+fun main() {
+    val list = listOf(1, 2, 3)
+    println(joinToString(list, prefix = "# ", postfix = ";"))
+    println(listOf("1", "2", "3").join(" "))
+}
+
+fun Collection<String>.join(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+)= joinToString(separator, prefix, postfix)
+
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+class NamedParameter {
+}
